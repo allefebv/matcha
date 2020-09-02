77910795e2b625c06a7e473718e10e1192a794c3
jfleury <jeremy@fleury.blue>
2020-09-02 19:06:22 +0200

    v.0.0.3 -> Header animation

--- src/root/Header.tsx
+++ src/root/Header.tsx
@@ -1,46 +1,46 @@
-import React from "react";
-import { Router } from "../component/Router";
-import { Login } from "../component/Login";
-import { SignUp } from "../component/SignUp";
-import { useWindowSize } from "../hooks/useWindowSize";
+import React from 'react';
+import { Router } from '../component/Router';
+import { Login } from '../component/Login';
+import { RegisterBar } from '../component/RegisterBar';
+import { useWindowSize } from '../hooks/useWindowSize';
 
-const styleContainerBlackStrip: React.CSSProperties = {
-    display: "flex",
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-    width: "100vw",
-    height: 80,
-    backgroundColor: "#0D0D0D",
+const styleHeader: React.CSSProperties = {
+	display: 'flex',
+	flexDirection: 'row',
+	alignItems: 'center',
+	justifyContent: 'space-between',
+	width: '100vw',
+	height: 80,
+	backgroundColor: '#0D0D0D',
 };
 
 interface Props {
-    router: string;
-    setRouter: React.Dispatch<React.SetStateAction<string>>;
-    user: null;
-    setUser: React.Dispatch<React.SetStateAction<null>>;
+	router: string;
+	setRouter: React.Dispatch<React.SetStateAction<string>>;
+	user: null;
+	setUser: React.Dispatch<React.SetStateAction<null>>;
 }
 
 export const Header = (props: Props) => {
-    const size = useWindowSize();
+	const size = useWindowSize();
 
-    return (
-        <div>
-            <div style={styleContainerBlackStrip}>
-                <Router
-                    router={props.router}
-                    setRouter={props.setRouter}
-                    size={size}
-                />
-                {props.user ? null : (
-                    <Login
-                        user={props.user}
-                        setUser={props.setUser}
-                        size={size}
-                    />
-                )}
-            </div>
-            {props.user ? null : <SignUp />}
-        </div>
-    );
+	return (
+		<div>
+			<div style={styleHeader}>
+				<Router
+					router={props.router}
+					setRouter={props.setRouter}
+					size={size}
+				/>
+				{props.user ? null : (
+					<Login
+						user={props.user}
+						setUser={props.setUser}
+						size={size}
+					/>
+				)}
+			</div>
+			{props.user ? null : <RegisterBar />}
+		</div>
+	);
 };
