983733b7e8abdfdd72c25fc4af912a6cf50ffde8
jfleury <jeremy@fleury.blue>
2020-10-20 15:40:00 +0200

    fix(script)

--- script/createUser/main.ts
+++ script/createUser/main.ts
@@ -6,7 +6,7 @@
 /*   By: jfleury <jfleury@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/08 18:41:14 by jfleury           #+#    #+#             */
-/*   Updated: 2020/10/12 16:27:14 by jfleury          ###   ########.fr       */
+/*   Updated: 2020/10/20 15:33:38 by jfleury          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,33 +25,29 @@ async function main() {
 	const userApiList: any = await getApiRandonUser();
 	const list = [];
 	let i = 0;
-	if (userApiList) {
-		await Promise.all(
-			userApiList.results.map(async (infoApi) => {
-				const user = await createUser(infoApi.email);
-				if (user) {
-					const profile = await createProfile(infoApi, user.token);
-					if (profile) {
-						const tag = await createTag(infoApi, user.token);
-						const location = await createLocation(infoApi, user.token);
-						const userProfile = {
-							user: user.user,
-							profile: profile,
-							location: location,
-							tag: tag,
-						};
-						i++;
-						list.push(userProfile);
-					}
-				}
-			})
-		);
-		fs.writeFile("userList.json", JSON.stringify(list), () => {
-			console.log(`Finish ${list.length} user create`);
-		});
-	} else {
-		console.log("userApiList null");
+	while (i < userApiList.results.length) {
+		const infoApi = userApiList.results[i];
+		const user = await createUser(infoApi.email);
+		if (user) {
+			const profile = await createProfile(infoApi, user.token);
+			if (profile) {
+				const tag = await createTag(infoApi, user.token);
+				const location = await createLocation(infoApi, user.token);
+				const userProfile = {
+					user: user.user,
+					profile: profile,
+					location: location,
+					tag: tag,
+				};
+				list.push(userProfile);
+			}
+		}
+		i++;
+		console.log("user ", i, " / ", HOW_MANY_CREATE_USER);
 	}
+	fs.writeFile("userList.json", JSON.stringify(list), () => {
+		console.log(`Finish ${list.length} user create`);
+	});
 }
 
 main();
