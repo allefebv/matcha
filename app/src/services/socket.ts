4987b6d9f551dbd3de879b717b13d0f38f99f2cd
jfleury <jeremy@fleury.blue>
2021-01-16 17:02:36 +0100

    fix(backend): merge socket.tsx

--- app/src/services/socket.ts
+++ app/src/services/socket.ts
@@ -6,11 +6,7 @@
 /*   By: jfleury <jfleury@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/22 09:44:14 by jfleury           #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2021/01/16 16:46:17 by jfleury          ###   ########.fr       */
-=======
-/*   Updated: 2021/01/15 16:19:06 by allefebv         ###   ########.fr       */
->>>>>>> deff0b24fa840f2132f5b0eb477507189e95f9ce
+/*   Updated: 2021/01/16 17:01:57 by jfleury          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,13 +22,8 @@ import { addMessage } from '../model/messageRepositories';
 import { handleNotifications } from './handleNotifications';
 
 export function socketRouter() {
-<<<<<<< HEAD
 	io.on('connection', (socket) => {
 		socket.on('online', async (payload: any) => {
-=======
-	io.on("connection", (socket) => {
-		socket.on("online", async (payload: any) => {
->>>>>>> deff0b24fa840f2132f5b0eb477507189e95f9ce
 			if (payload.id) {
 				try {
 					io.emit('online', payload.id);
@@ -71,11 +62,7 @@ export function socketRouter() {
 				);
 			}
 		});
-<<<<<<< HEAD
 		socket.on('disconnect', async () => {
-=======
-		socket.on("disconnect", async () => {
->>>>>>> deff0b24fa840f2132f5b0eb477507189e95f9ce
 			try {
 				io.emit('offline', socket['userId']);
 				const profile = await getProfileByUserId(socket['userId']);
